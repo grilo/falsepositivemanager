c048a2fcdd7371b753232af991c23e1b84547d44
grilo <joao.grilo@gmail.com>
2015-12-01 03:03:20 +0100

    More refactoring, removing large amounts of unneeded code.

--- static/app.js
+++ static/app.js
@@ -49,86 +49,63 @@ function renderPage(anchor) {
             break;
 
         case "#submit":
-            var panel = new UploadForm("Upload file", "Choose a file from your computer", "Choose file");
-            content.appendChild(panel.toHTML());
+            getTemplate("submit", function (tpl) {
+                var tplData = {
+                    "title": "Upload files",
+                };
+                var node = jsRender(tpl, tplData);
+                content.appendChild(node);
 
-            var errBox = document.getElementById('footertext');
-            var btn = document.getElementById("upload-btn");
+                var errBox = document.getElementById('footertext');
+                var btn = document.getElementById("upload-btn");
 
-            var uploader = new ss.SimpleUpload({
-                button: 'upload-btn', // HTML element used as upload button
-                dropzone: 'main-panel',
-                url: '/review', // URL of server-side upload handler
-                name: 'uploadfile', // Parameter name of the uploaded file
-                multipart: true,
-                hoverClass: 'btn-hover',
-                focusClass: 'active',
-                disabledClass: 'disabled',
-                responseType: 'json',
+                var uploader = new ss.SimpleUpload({
+                    button: 'upload-btn', // HTML element used as upload button
+                    dropzone: 'main-panel',
+                    url: '/review', // URL of server-side upload handler
+                    name: 'uploadfile', // Parameter name of the uploaded file
+                    multipart: true,
+                    hoverClass: 'btn-hover',
+                    focusClass: 'active',
+                    disabledClass: 'disabled',
+                    responseType: 'json',
 
-                onSubmit: function(filename, ext) {            
-                        this.setProgressBar(document.getElementById('progress-bar'));
-                        this.setFileSizeBox(document.getElementById('progress-percent'));
-                },        
-                onComplete: function(file, response, btn) {            
-                    if (!response) {
-                        errBox.innerHTML = 'Unable to upload file';
-                    } else {
-                        errBox.innerHTML = 'Upload OK!';
-                    }
-                    if (response.success === true) {
-                        errBox.innerHTML = 'Upload OK!';
-                    } else {
-                        if (response.msg)  {
-                            errBox.innerHTML = response.msg;
-                        } else {
+                    onSubmit: function(filename, ext) {            
+                            this.setProgressBar(document.getElementById('progress-bar'));
+                            this.setFileSizeBox(document.getElementById('progress-percent'));
+                    },        
+                    onComplete: function(file, response, btn) {            
+                        if (!response) {
                             errBox.innerHTML = 'Unable to upload file';
+                        } else {
+                            errBox.innerHTML = 'Upload OK!';
+                        }
+                        if (response.success === true) {
+                            errBox.innerHTML = 'Upload OK!';
+                        } else {
+                            if (response.msg)  {
+                                errBox.innerHTML = response.msg;
+                            } else {
+                                errBox.innerHTML = 'Unable to upload file';
+                            }
                         }
                     }
-                }
+                });
+
             });
+
 
             break;
         case "#running":
             getRunning().success(function (response) {
-                var panel = new BPanel();
-                panel.setContext("panel-primary");
-                panel.setHeader("Running Tasks");
-                panel = panel.toHTML();
-
-                var table = new JSONTable(response)
-                table = table.toHTML(function (tr) {
-                    // Insert Action into the table header
-                    if (tr.parentElement.nodeName == "THEAD") {
-                        tr.insertCell().innerHTML = "Action";
-                    } else {
-                        // Otherwise just place a review button
-                        var td = tr.insertCell();
-                        // We know the ID is the first cell
-                        var id = tr.getElementsByTagName('td')[0].innerHTML;
-                        td.appendChild(review_button(id));
-                    }
-                }, function (td) {
-                    // Color code according to severity
-                    if (td.innerHTML.match(/^CVE-/)) {
-                        var id = td.innerHTML;
-                        td.innerHTML = "";
-                        var a = document.createElement("a");
-                        a.href = 'https://web.nvd.nist.gov/view/vuln/detail?vulnId=' + id;
-                        a.innerHTML = id;
-                        td.appendChild(a)
-                    } else if (td.innerHTML.match(/^CWE-/)) {
-                        var id = td.innerHTML;
-                        td.innerHTML = "";
-                        var a = document.createElement("a");
-                        a.href = 'http://cwe.mitre.org/data/definitions/' + id.split(" ")[0].split("-")[1] + '.html';
-                        a.innerHTML = id;
-                        td.appendChild(a)
-                    }
+                getTemplate("running", function (tpl) {
+                    var tplData = {
+                        "title": "Running Tasks",
+                        "object": response,
+                    };
+                    var node = jsRender(tpl, tplData);
+                    content.appendChild(node);
                 });
-                table.className = "table table-striped table-hover";
-                panel.appendChild(table)
-                content.appendChild(panel);
             });
             break;
         case "#history":
@@ -163,28 +140,13 @@ function renderPage(anchor) {
                                 tplDependency(expandElement, dependency);
                             });
                         });
-                    });
+                    }); // End of preload
                 });
             });
             break;
         case "#admin":
-            var panel = new BPanel();
-            panel.setContext("panel-primary");
-            panel.setHeader("Admin");
-            panel.setCaption("These changes are not reversible.");
-            panel = panel.toHTML();
-
-            var accept_all = document.createElement("a");
-            panel.appendChild(accept_all);
-            accept_all.href = "#";
-            accept_all.className = "btn btn-large btn-block btn-primary";
-            accept_all.text = "Accept all pending reviews";
-
-            var reject_all = document.createElement("a");
-            panel.appendChild(reject_all);
-            reject_all.href = "#";
-            reject_all.className = "btn btn-large btn-block btn-success";
-            reject_all.text = "Reject all pending reviews";
+            var panel = document.createElement("div");
+            panel.innerHTML = "<h1>Hello world</h1>";
             content.appendChild(panel);
             break;
     }
